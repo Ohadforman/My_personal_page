b34df3380238f57bf6faf93704690ce0866c763b
Ohad Forman <ohadforman@mail.tau.ac.il>
2024-01-08 00:30:24 +0200

    Fix version mismatch

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-import os
+
 app = Flask(__name__)
 
 
@@ -86,4 +86,4 @@ def cv():
     return render_template('cv.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, port=4070)
