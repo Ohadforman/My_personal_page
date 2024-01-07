cb6d8de0bb361f4cfef8250e896861ecb23adf4f
Ohad Forman <ohadforman@mail.tau.ac.il>
2024-01-07 20:38:47 +0200

    1

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 # Replace this with your actual degree scores data
 degree_data = {
